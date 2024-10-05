9e4a4e78d0c52b4ad1fe9e3d50a61f846540d509
Rianda <e41231605@student.polije.ac.id>
2024-10-05 12:03:12 +0700

    mengubah code fragment program institusi java

--- app/src/main/java/com/example/projectpelatihanku/FragmentProgramInstitusi.java
+++ app/src/main/java/com/example/projectpelatihanku/FragmentProgramInstitusi.java
@@ -1,11 +1,11 @@
 package com.example.projectpelatihanku;
 
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView; // Pastikan untuk mengimpor TextView
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -18,7 +18,7 @@ public class FragmentProgramInstitusi extends Fragment {
         // Inflate layout untuk fragment
         View view = inflater.inflate(R.layout.fragment_program_institusi, container, false); // Ganti dengan layout Anda
 
-        // Inisialisasi ImageView
+        // Inisialisasi ImageView untuk navigasi ke FragmentInstitusi
         ImageView imageArrow = view.findViewById(R.id.imageArrow2);
         imageArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +27,18 @@ public class FragmentProgramInstitusi extends Fragment {
                 MainActivity mainActivity = (MainActivity) getActivity();
                 if (mainActivity != null) {
                     mainActivity.navigateToInstitusi(); // Memanggil metode navigasi ke FragmentInstitusi
+                }
+            }
+        });
+
+        // Inisialisasi TextView untuk navigasi ke FragmentDetailProgram
+        TextView btnDetail = view.findViewById(R.id.btn_detail); // Pastikan ID ini sesuai dengan layout XML
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity mainActivity = (MainActivity) getActivity();
+                if (mainActivity != null) {
+                    mainActivity.navigateToDetailProgram(); // Memanggil metode untuk berpindah ke FragmentDetailProgram
                 }
             }
         });
