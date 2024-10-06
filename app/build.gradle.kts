8ab33339b539523295391e7fcd10ddf0515d01ca
Rianda <e41231605@student.polije.ac.id>
2024-10-06 21:24:38 +0700

    mengedit build gradle kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
@@ -35,11 +36,15 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.play.services.location)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location.license)
+    implementation(libs.gms.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
