afb3d8d29ebb0fe88b5b3ed8c5b186cbab4ed346
Rianda <e41231605@student.polije.ac.id>
2024-10-29 16:03:10 +0700

    memperbarui build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
-    implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation(libs.firebase.firestore)
     implementation ("com.google.android.material:material:1.9.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
@@ -56,4 +53,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // okHttp
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.google.code.gson:gson:2.8.8")
+
+
 }
