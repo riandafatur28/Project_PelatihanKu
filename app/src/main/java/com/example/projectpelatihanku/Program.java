d4480e140dd5fb5003f1c13565c68b73de2a669c
Rianda <e41231605@student.polije.ac.id>
2024-10-29 15:59:42 +0700

    menambahkan program java

---
+++ app/src/main/java/com/example/projectpelatihanku/Program.java
@@ -0,0 +1,33 @@
+package com.example.projectpelatihanku;
+
+import java.io.Serializable;
+
+public class Program implements Serializable {
+    private String id; // Menambahkan ID untuk institusi
+    private String nama;
+    private String deskripsi;
+    private String imageUrl;
+
+    public Program(String id, String nama, String deskripsi, String imageUrl) {
+        this.id = id;
+        this.nama = nama;
+        this.deskripsi = deskripsi;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
