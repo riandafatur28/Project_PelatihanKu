50009493c05ec8b76b64f2246ee413d9e8a2afdd
Rianda <e41231605@student.polije.ac.id>
2024-10-31 08:53:58 +0700

    update coding myNotification java

--- app/src/main/java/com/example/projectpelatihanku/MyNotification.java
+++ app/src/main/java/com/example/projectpelatihanku/MyNotification.java
@@ -3,11 +3,13 @@ package com.example.projectpelatihanku;
 public class MyNotification {
     private String title;
     private boolean isRead;
-    private boolean checked; // Untuk status CheckBox
+    private boolean checked;// Untuk status CheckBox
+    private String id;
 
-    public MyNotification(String title, boolean isRead) {
+    public MyNotification(String id, String title, boolean isRead) {
         this.title = title;
         this.isRead = isRead;
+        this.id = id;
         this.checked = false; // Default tidak dicentang
     }
 
