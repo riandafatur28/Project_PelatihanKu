6012274d11856af8e3088dca6b9ddd580d82bb75
Rianda <e41231605@student.polije.ac.id>
2024-11-05 17:27:56 +0700

    update fragment login

--- app/src/main/java/com/example/projectpelatihanku/FragmentLogin.java
+++ app/src/main/java/com/example/projectpelatihanku/FragmentLogin.java
@@ -1,5 +1,7 @@
 package com.example.projectpelatihanku;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
@@ -10,34 +12,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.auth0.android.jwt.JWT;
 import com.example.projectpelatihanku.api.ApiClient;
-import com.example.projectpelatihanku.api.CallBackHelper;
+
 
 import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class FragmentLogin extends Fragment {
     private Button buttonLogin;
     private TextView textRegister, textForgotPassword;
     private boolean isPasswordVisible = false;
-    private static final String PREFS_NAME = "UserPrefs";
-    private static final String KEY_EMAIL = "email";
-    private static final String KEY_PASSWORD = "password";
     private EditText emailEditText, passwordEditText;
     private ImageView iconPassword;
-    public static String sayName;
-    public static String sayTtl;
-    public static String sayEmail;
-    public static String sayNoTelp;
-    public static String sayJenisKelamin;
-    public static String sayAlamat;
+
+    static String Name;
+    static String firstName;
 
     // End Point
-    private String endPoint = "/login";
+    private String endPoint = "login";
 
     @Nullable
     @Override
@@ -59,50 +59,30 @@ public class FragmentLogin extends Fragment {
 
             ApiClient apiClient = new ApiClient();
             try {
-                apiClient.oauthLogin(endPoint, email, password, new CallBackHelper() {
+                apiClient.oauthLogin(endPoint, email, password, new ApiClient.LoginHelper() {
                     @Override
-                    public void onLoginSuccess(String name) {
-                        if (getActivity() != null) {
-                            getActivity().runOnUiThread(() -> {
-                                if (getActivity() instanceof MainActivity) {
-                                    sayName = name;
-                                    sayEmail = email;
-                                    ((MainActivity) getActivity()).navigateToDashboard();
-                                } else {
-                                    Log.e("FragmentLogin", "MainActivity not found");
-                                }
-                            });
-                        }
+                    public void onSuccess(String token) {
+                        getActivity().runOnUiThread(() -> {
+                            SharedPreferences preferences = getActivity().getSharedPreferences("accountToken", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            editor.putString("token", token);
+                            editor.apply();
+                            JWT jwt = new JWT(token);
+                            String username = jwt.getClaim("users").asObject(Map.class).get("username").toString();
+                            Name = username;
+                            firstName = Name.split(" ")[0];
+                            if (getActivity() instanceof MainActivity) {
+                                ((MainActivity) getActivity()).navigateToDashboard();
+                            } else {
+                                Log.e("FragmentLogin", "MainActivity not found");
+                            }
+                        });
+
                     }
 
                     @Override
-                    public void onLoginFailed() {
-                        if (getActivity() != null) {
-                            getActivity().runOnUiThread(() ->
-                                    Toast.makeText(getContext(), "Username atau password salah", Toast.LENGTH_LONG).show()
-                            );
-                        }
-                    }
-
-                    @Override
-                    public void onProfileFetchSuccess(String name, String email, String phoneNumber, String gender, String ttl, String alamat) {
-                        // Tidak digunakan di FragmentLogin, jadi biarkan kosong
-                        if (getActivity() instanceof MainActivity) {
-                            sayName = name;
-                            sayEmail = email;
-                            sayNoTelp = phoneNumber;
-                            sayJenisKelamin = gender;
-                            sayTtl = ttl;
-                            sayAlamat = alamat;
-                            ((MainActivity) getActivity()).navigateToProfil();
-                        } else {
-                            Log.e("FragmentProfil", "MainActivity not found");
-                        }
-                    }
-
-                    @Override
-                    public void onProfileFetchFailure(String errorMessage) {
-
+                    public void onFailed(IOException e) {
+                        Log.d("Failed", "onFailed " + e.getMessage());
                     }
                 });
             } catch (JSONException e) {
@@ -144,15 +124,5 @@ public class FragmentLogin extends Fragment {
                 InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD :
                 InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         passwordEditText.setSelection(passwordEditText.length());
-    }
-
-    // Validasi email agar berformat email dan diakhiri dengan "@gmail.com"
-    private boolean isValidEmail(String email) {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() && email.endsWith("@gmail.com");
-    }
-
-    // Validasi password panjangnya 8 karakter, ada huruf kapital, angka, dan tidak ada spasi
-    private boolean isValidPassword(String password) {
-        return password.length() == 8 && password.matches(".*[A-Z].*") && password.matches(".*[0-9].*") && !password.contains(" ");
     }
 }
