e5d50f1ad1020875e17f1f80f693bf6044dff5c7
Rianda <e41231605@student.polije.ac.id>
2024-11-06 16:19:01 +0700

    mengubah nama FragmentLupaSandi.java menjadi FragmentForgotPassword.java

--- app/src/main/java/com/example/projectpelatihanku/FragmentForgotPassword.java
+++ app/src/main/java/com/example/projectpelatihanku/FragmentForgotPassword.java
@@ -1,7 +1,6 @@
 package com.example.projectpelatihanku;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,7 +28,7 @@ public class FragmentForgotPassword extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_lupa_sandi, container, false);
+        View view = inflater.inflate(R.layout.fragment_forgot_password, container, false);
 
         buttonKirim = view.findViewById(R.id.button_kirim);
         txtKembali = view.findViewById(R.id.txtKembali);
