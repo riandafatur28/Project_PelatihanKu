8c084d8cfdcbc9f1265db76572d57001c7fbee41
Rianda <e41231605@student.polije.ac.id>
2024-11-06 16:04:20 +0700

    mengubah nama FragmentInstitusi.java.java menjadi FragmentDepartment.java

--- app/src/main/java/com/example/projectpelatihanku/FragmentDepartment.java
+++ app/src/main/java/com/example/projectpelatihanku/FragmentDepartment.java
@@ -7,33 +7,37 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.projectpelatihanku.api.ApiClient;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FragmentInstitusi extends Fragment {
+public class FragmentDepartment extends Fragment {
     private String endPoint = "/departments";
     private RecyclerView recyclerView;
-    private InstitusiAdapter adapter;
-    private List<Institusi> institusiList = new ArrayList<>();
+    private DepartmentAdapter adapter;
+    private List<Department> departmentList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_institusi, container, false);
+        View view = inflater.inflate(R.layout.fragment_department, container, false);
 
         recyclerView = view.findViewById(R.id.recyclerViewInstitusi);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        adapter = new InstitusiAdapter(institusiList, getContext());
+        adapter = new DepartmentAdapter(departmentList, getContext());
         recyclerView.setAdapter(adapter);
 
         fetchData();
         return view;
     }
+
     public void fetchData() {
         // Get token dari shared preference
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("accountToken", Context.MODE_PRIVATE);
@@ -43,11 +47,11 @@ public class FragmentInstitusi extends Fragment {
         // get data
         apiClient.fetchDepartment(token, endPoint, new ApiClient.DepartmentHelper() {
             @Override
-            public void onSuccess(ArrayList<Institusi> data) {
+            public void onSuccess(ArrayList<Department> data) {
                 requireActivity().runOnUiThread(() -> {
-                    institusiList.clear();
-                    institusiList.addAll(data);
-                    Log.d("Department", "onSuccessFetchDepartment: " +data);
+                    departmentList.clear();
+                    departmentList.addAll(data);
+                    Log.d("Department", "onSuccessFetchDepartment: " + data);
                     adapter.notifyDataSetChanged();
                 });
             }
