8bd62e80b46acc9c3b12a2d3c063a77226e2deee
Rianda <e41231605@student.polije.ac.id>
2024-11-06 16:08:59 +0700

    memperbarui ConfirmPasswordFragment.java

--- app/src/main/java/com/example/projectpelatihanku/ConfirmPasswordFragment.java
+++ app/src/main/java/com/example/projectpelatihanku/ConfirmPasswordFragment.java
@@ -1,13 +1,17 @@
 package com.example.projectpelatihanku;
 
 import androidx.lifecycle.ViewModelProvider;
+
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.projectpelatihanku.ui.main.MainViewModel;
 
 public class ConfirmPasswordFragment extends Fragment {
