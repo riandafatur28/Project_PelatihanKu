a0f4c34567dd52f69be151de7b52d4527e9de922
Rianda <e41231605@student.polije.ac.id>
2024-11-06 16:06:47 +0700

    mengubah nama InstitusiAdapter.java menjadi DepartmentAdapter.java

--- app/src/main/java/com/example/projectpelatihanku/DepartmentAdapter.java
+++ app/src/main/java/com/example/projectpelatihanku/DepartmentAdapter.java
@@ -14,34 +14,34 @@ import com.bumptech.glide.Glide;
 import com.google.android.material.imageview.ShapeableImageView;
 import java.util.List;
 
-public class InstitusiAdapter extends RecyclerView.Adapter<InstitusiAdapter.InstitusiViewHolder> {
-    private List<Institusi> institusiList;
+public class DepartmentAdapter extends RecyclerView.Adapter<DepartmentAdapter.InstitusiViewHolder> {
+    private List<Department> institusiList;
     private Context context;
 
-    public InstitusiAdapter(List<Institusi> institusiList, Context context) {
-        this.institusiList = institusiList;
+    public DepartmentAdapter(List<Department> departmentList, Context context) {
+        this.institusiList = departmentList;
         this.context = context;
     }
 
     @NonNull
     @Override
     public InstitusiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_institusi, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_department, parent, false);
         return new InstitusiViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull InstitusiViewHolder holder, int position) {
-        Institusi institusi = institusiList.get(position);
-        holder.textNamaInstitusi.setText(institusi.getNama()); // Memanggil metode yang benar
-        holder.textDeskripsi.setText(institusi.getDeskripsi());
-        Glide.with(context).load(institusi.getImageUrl()).into(holder.imageInstitusi);
+        Department department = institusiList.get(position);
+        holder.textNamaInstitusi.setText(department.getNama()); // Memanggil metode yang benar
+        holder.textDeskripsi.setText(department.getDeskripsi());
+        Glide.with(context).load(department.getImageUrl()).into(holder.imageInstitusi);
         // Menambahkan listener untuk tombol lihat program
         holder.btnLihatProgram.setOnClickListener(v -> {
-//            FragmentProgramInstitusi fragmentProgramInstitusi = FragmentProgramInstitusi.newInstance(institusi);
+            FragmentProgram fragmentProgramInstitusi = FragmentProgram.newInstance(String.valueOf(department));
             FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//            fragmentTransaction.replace(R.id.fragment_container, fragmentProgramInstitusi);
+            fragmentTransaction.replace(R.id.fragment_container, fragmentProgramInstitusi);
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         });
