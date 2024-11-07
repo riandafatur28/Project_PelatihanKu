2e5f2e75fb1ae2b3d9d4eb770a0edd5781c85818
Rianda <e41231605@student.polije.ac.id>
2024-11-07 19:11:43 +0700

    memperbarui DashboardFragment.java

--- app/src/main/java/com/example/projectpelatihanku/DashboardFragment.java
+++ app/src/main/java/com/example/projectpelatihanku/DashboardFragment.java
@@ -6,11 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -19,7 +17,6 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
