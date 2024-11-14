cee4d0a110287b86f142de51c355d4030e4230bc
Rianda <e41231605@student.polije.ac.id>
2024-11-14 19:43:21 +0700

    update Program.java

--- app/src/main/java/com/example/projectpelatihanku/Program.java
+++ app/src/main/java/com/example/projectpelatihanku/Program.java
@@ -2,19 +2,23 @@ package com.example.projectpelatihanku;
 
 import java.io.Serializable;
 
-public class Program implements Serializable {
+public class Program implements  Serializable{
     private String id;
     private String nama;
     private String deskripsi;
     private String imageUrl;
+    private String departmentId;
 
+    // Constructor
     public Program(String id, String nama, String deskripsi, String imageUrl) {
         this.id = id;
         this.nama = nama;
         this.deskripsi = deskripsi;
         this.imageUrl = imageUrl;
+        this.departmentId = departmentId;
     }
 
+    // Getters
     public String getId() {
         return id;
     }
@@ -29,5 +33,9 @@ public class Program implements Serializable {
 
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
     }
 }
