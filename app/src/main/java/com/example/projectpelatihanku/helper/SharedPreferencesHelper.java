c967944a6d5e1c053258e738580e3eacf9d7c477
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-16 23:16:16 +0700

    menambahkan javadoc untuk kelas SharedPreferencesHelper

--- app/src/main/java/com/example/projectpelatihanku/helper/SharedPreferencesHelper.java
+++ app/src/main/java/com/example/projectpelatihanku/helper/SharedPreferencesHelper.java
@@ -3,9 +3,17 @@ package com.example.projectpelatihanku.helper;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/**
+ * Class untuk mengelola interaksi dengan shared preferences
+ */
 public class SharedPreferencesHelper {
     private static final String PREF_NAME = "accountToken";
 
+    /**
+     * Menyimpan token JWT ke shared preferences
+     * @param context konteks aplikasi
+     * @param token token JWT
+     */
     public static void saveToken(Context context, String token) {
         SharedPreferences preferences = context.getSharedPreferences(PREF_NAME,
                 Context.MODE_PRIVATE);
