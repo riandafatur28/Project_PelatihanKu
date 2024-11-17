60cf7020b97a3bf063183351d2df56269e1047d1
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-18 02:12:51 +0700

    fix code di FragmentForgotPassword.java

--- app/src/main/java/com/example/projectpelatihanku/FragmentForgotPassword.java
+++ app/src/main/java/com/example/projectpelatihanku/FragmentForgotPassword.java
@@ -1,82 +1,106 @@
 package com.example.projectpelatihanku;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.projectpelatihanku.api.ApiClient;
+import com.example.projectpelatihanku.helper.FragmentHelper;
 
 import java.io.IOException;
 
 public class FragmentForgotPassword extends Fragment {
 
     private Button buttonKirim;
-    private LinearLayout txtKembali;
+    private Button btnBack;
     private EditText editTextEmail;
-    private ApiClient apiClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forgot_password, container, false);
 
-        apiClient = new ApiClient();
-
         buttonKirim = view.findViewById(R.id.button_kirim);
-        txtKembali = view.findViewById(R.id.txtKembali);
+        btnBack = view.findViewById(R.id.btnBack);
         editTextEmail = view.findViewById(R.id.inputEmail);
 
         buttonKirim.setOnClickListener(v -> {
             String email = editTextEmail.getText().toString().trim();
-
-            if (email.isEmpty()) {
-                Toast.makeText(getActivity(), "Email tidak boleh kosong.", Toast.LENGTH_LONG).show();
-            } else if (!email.endsWith("@gmail.com")) {
-                Toast.makeText(getActivity(), "Masukkan email yang valid dengan domain @gmail.com", Toast.LENGTH_LONG).show();
-            } else {
-                sendPasswordResetRequest(email);
+            if (validateEmail(email)) {
+                sendRequestOTP(new ApiClient(), email);
             }
         });
 
-        txtKembali.setOnClickListener(v -> {
-            if (getActivity() instanceof MainActivity) {
-                ((MainActivity) getActivity()).navigateToLogin();
-            }
-        });
+        backButtonHandler(view);
 
         return view;
     }
 
-    private void sendPasswordResetRequest(String email) {
-
-        apiClient.requestPasswordReset(email, new ApiClient.PasswordResetHelper() {
+    /**
+     * Mengirimkan request OTP ke server
+     *
+     * @param apiClient Service Class untuk mengirim request ke server
+     * @param email     alamat email untuk kirim kode OTP
+     * @see ApiClient#sendRequestOtp(String, ApiClient.RequestResetPassword)
+     * @see FragmentHelper#navigateToFragment(FragmentActivity, int, Fragment, boolean, String)
+     */
+    private void sendRequestOTP(ApiClient apiClient, String email) {
+        apiClient.sendRequestOtp(email, new ApiClient.RequestResetPassword() {
             @Override
             public void onSuccess(String tempToken) {
-
-                if (getActivity() != null) {
-                    getActivity().runOnUiThread(() -> {
-                        Toast.makeText(getActivity(), "Kode OTP dikirim, cek email Anda.", Toast.LENGTH_LONG).show();
-                        if (getActivity() instanceof MainActivity) {
-                            ((MainActivity) getActivity()).navigateToOTP();
-                        }
-                    });
-                }
+                getActivity().runOnUiThread(() -> {
+                    Toast.makeText(getActivity(), "Kode OTP berhasil dikirim, Cek email anda.",
+                            Toast.LENGTH_LONG).show();
+                    FragmentHelper.navigateToFragment(getActivity(), R.id.navActivity,
+                            new FragmentOTP(), true, null);
+                });
             }
 
             @Override
             public void onFailed(IOException e) {
                 if (getActivity() != null) {
                     getActivity().runOnUiThread(() ->
-                            Toast.makeText(getActivity(), "Request gagal: " + e.getMessage(), Toast.LENGTH_LONG).show()
+                            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show()
                     );
                 }
             }
         });
+    }
+
+    /**
+     * Handler untuk tombol kembali
+     *
+     * @param view View tempat tombol kembali berada
+     * @see FragmentHelper#backNavigation(FragmentActivity, ImageView, Button)
+     */
+    private void backButtonHandler(View view) {
+        btnBack = view.findViewById(R.id.btnBack);
+        FragmentHelper.backNavigation(getActivity(), null, btnBack);
+    }
+
+    /**
+     * Validasi email sebelum dikirim ke server
+     * domain email yang diterima adalah @gmail.com
+     *
+     * @param email alamat email yang akan diperiksa
+     */
+    private Boolean validateEmail(String email) {
+        if (email.isEmpty()) {
+            Toast.makeText(getActivity(), "Email tidak boleh kosong", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (!email.endsWith("@gmail.com")) {
+            Toast.makeText(getActivity(), "Masukkan email yang valid dengan domain @gmail.com", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
