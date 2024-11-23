4c482e29db596078d9f02b9f5b585bafd3e70dc9
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-23 22:36:34 +0700

    membuat metode handler untuk koneksi websocket dan http request untuk memperbarui value is_read dan is_deleted dari notifikasi

--- app/src/main/java/com/example/projectpelatihanku/FragmentNotifikasi.java
+++ app/src/main/java/com/example/projectpelatihanku/FragmentNotifikasi.java
@@ -1,132 +1,156 @@
 package com.example.projectpelatihanku;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.Toast;
 
 import com.auth0.android.jwt.JWT;
+import com.example.projectpelatihanku.Models.Notification;
 import com.example.projectpelatihanku.api.ApiClient;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.example.projectpelatihanku.helper.SharedPreferencesHelper;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class FragmentNotifikasi extends Fragment implements OnNotificationCheckedChangeListener {
-    private static final String PREFS_NAME = "NotificationPrefs";
-    private static final String KEY_NOTIFICATIONS = "cachedNotifications";
-    private String endPoint = "/notifications";
+public class FragmentNotifikasi extends Fragment {
     private NotificationAdapter adapter;
-    private List<MyNotification> notifications = new ArrayList<>();
-    private Button buttonHapus;
+    private RecyclerView recyclerView;
+    private List<Notification> notifications = new ArrayList<>();
+    private ApiClient apiClient;
+    private static String token;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_notifikasi, container, false);
+        recyclerView = view.findViewById(R.id.recyclerViewNotifikasi);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        ListView listViewNotifikasi = view.findViewById(R.id.listViewNotifikasi);
-        buttonHapus = view.findViewById(R.id.buttonHapus);
-
-        // Set adapter dengan data yang ada
-        adapter = new NotificationAdapter(requireContext(), notifications, this);
-        listViewNotifikasi.setAdapter(adapter);
-
-        // Fetch data terbaru di latar belakang untuk memperbarui notifikasi
-
-        listViewNotifikasi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                MyNotification selectedNotification = notifications.get(position);
-                selectedNotification.markAsRead(); // Tandai sebagai sudah dibaca
-
-                adapter.notifyDataSetChanged(); // Perbarui tampilan adapter
-
-                FragmentDetailNotifikasi detailFragment = FragmentDetailNotifikasi.newInstance(
-                        selectedNotification.getTitle(),
-                        "Detail untuk notifikasi: " + selectedNotification.getTitle()
-                );
-                FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.layerNotifikasi, detailFragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-            }
-        });
-
-        buttonHapus.setOnClickListener(v -> deleteSelectedNotifications());
-        updateDeleteButtonVisibility();
-
-        fetchData();
+        adapter = new NotificationAdapter(getContext(), notifications);
+        recyclerView.setAdapter(adapter);
+        token = SharedPreferencesHelper.getToken(getContext());
+        connectWebSocket();
 
         return view;
     }
 
-    @Override
-    public void onNotificationCheckedChange() {
-        updateDeleteButtonVisibility();
+    /**
+     * Digunakan untuk melakukan koneksi ke websocket
+     *
+     * @see ApiClient#fetchNotification(String, int, String, ApiClient.WebSocketCallback)
+     */
+    private void connectWebSocket() {
+        JWT jwt = new JWT(token);
+        Double userIdDouble = (Double) jwt.getClaim("users").asObject(Map.class).get("id");
+        int userId = userIdDouble.intValue();
+        apiClient = new ApiClient();
+        apiClient.fetchNotification(token, userId, "ws://192.168.100.4:8000/notifications",
+                new ApiClient.WebSocketCallback() {
+                    @Override
+                    public void onMessageReceived(List<Notification> data) {
+                        requireActivity().runOnUiThread(() -> {
+                            for (Notification newNotification : data) {
+                                boolean isDuplicate = false;
+                                for (Notification existingNotification : notifications) {
+                                    if (existingNotification.getId().equals(newNotification.getId())) {
+                                        isDuplicate = true;
+                                        break;
+                                    }
+                                }
+                                if (!isDuplicate) {
+                                    notifications.add(0, newNotification);
+                                }
+                            }
+                            adapter.notifyDataSetChanged();
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(IOException e) {
+                        requireActivity().runOnUiThread(() -> {
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        });
+                    }
+                });
     }
 
-    private void deleteSelectedNotifications() {
-        Iterator<MyNotification> iterator = notifications.iterator();
-        while (iterator.hasNext()) {
-            MyNotification notification = iterator.next();
-            if (notification.isChecked()) {
-                iterator.remove();
-            }
-        }
-        adapter.notifyDataSetChanged();
-        updateDeleteButtonVisibility();
-    }
-
-    private void updateDeleteButtonVisibility() {
-        boolean anyChecked = false;
-        for (MyNotification notification : notifications) {
-            if (notification.isChecked()) {
-                anyChecked = true;
-                break;
-            }
-        }
-        buttonHapus.setVisibility(anyChecked ? View.VISIBLE : View.GONE);
-    }
-
-    public void fetchData() {
-        // Get token dari shared preference
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("accountToken", Context.MODE_PRIVATE);
-        String token = sharedPreferences.getString("token", "Token Tidak ditemukan");
-        ApiClient apiClient = new ApiClient();
-
-        // get data
-        apiClient.fetchNotifikasi(token, endPoint, new ApiClient.NotifikasiHelper() {
-
+    /**
+     * Digunakan mengupdate status notifikasi is_read = 1 (true)
+     *
+     * @param notification Notifikasi yang akan diupdate
+     * @param apiClient    Service class untuk melakukan request ke server
+     * @param context      Context dari fragment yang memanggil method
+     * @see ApiClient#isReadNotification(String, String, ApiClient.notificationUpdateHelper)
+     */
+    public static void updateIsRead(Notification notification, ApiClient apiClient, Context context) {
+        String nofiticationId = notification.getId();
+        String endPoint = "/notifications/is-read/" + nofiticationId;
+        apiClient.isReadNotification(token, endPoint, new ApiClient.notificationUpdateHelper() {
             @Override
-            public void onSuccess(ArrayList<MyNotification> data) {
-                requireActivity().runOnUiThread(() -> {
-                    notifications.clear();
-                    notifications.addAll(data);
-                    adapter.notifyDataSetChanged();
+            public void onSuccess(String message) {
+                ((FragmentActivity) context).runOnUiThread(() -> {
+                    Log.d("Updated", "message: " + message);
                 });
             }
 
             @Override
             public void onFailed(IOException e) {
-
+                ((FragmentActivity) context).runOnUiThread(() -> {
+                    Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
             }
         });
+    }
+
+    /**
+     * Digunakan mengupdate status notifikasi is_deleted = 1 (true)
+     *
+     * @param notification Notifikasi yang akan diupdate
+     * @param apiClient    Service class untuk melakukan request ke server
+     * @param context      Context dari fragment yang memanggil method
+     * @see ApiClient#isDeletedNotification(String, String, ApiClient.notificationUpdateHelper)
+     */
+    public static void updateIsDeleted(Notification notification, ApiClient apiClient,
+                                       Context context) {
+        String nofiticationId = notification.getId();
+        String endPoint = "/notifications/is-deleted/" + nofiticationId;
+        apiClient.isDeletedNotification(token, endPoint, new ApiClient.notificationUpdateHelper() {
+            @Override
+            public void onSuccess(String message) {
+                ((FragmentActivity) context).runOnUiThread(() -> {
+                    Log.d("Updated", "message: " + message);
+                });
+            }
+
+            @Override
+            public void onFailed(IOException e) {
+                ((FragmentActivity) context).runOnUiThread(() -> {
+                    Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (apiClient != null) {
+            apiClient.disconnectWebSocket();
+        }
     }
 }
